9d92ea71f390d12a9ea13aed0a5253551fbe2874
moiz.akhtar <moiz.akhtar@pitb.gov.pk>
2024-06-22 21:58:20 +0500

    ESC Tasks 22-06-2024

--- public/build/assets/InputLabel-9021cf40.js
+++
@@ -1,1 +0,0 @@
-import{o as t,f as e,t as o,r as l}from"./app-0dc89f51.js";const n={class:"block font-light text-sm text-gray-700"},r={key:0},c={key:1},p={__name:"InputLabel",props:["value"],setup(s){return(a,_)=>(t(),e("label",n,[s.value?(t(),e("span",r,o(s.value),1)):(t(),e("span",c,[l(a.$slots,"default")]))]))}};export{p as _};
