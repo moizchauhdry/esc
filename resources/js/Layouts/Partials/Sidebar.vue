04bf87946a7a6c9ba441bcf0a189383d6a0d9b13
moiz.akhtar <moiz.akhtar@pitb.gov.pk>
2024-06-25 22:12:44 +0500

    sidebar toggle and child menu

--- resources/js/Layouts/Partials/Sidebar.vue
+++ resources/js/Layouts/Partials/Sidebar.vue
@@ -1,6 +1,50 @@
 <script setup>
 import { Link, usePage } from "@inertiajs/vue3";
+import { ref } from "vue";
+import { computed, onMounted, onUnmounted, watch } from 'vue';
+
 const permission = usePage().props.can;
+
+const props = defineProps({
+    closeable: {
+        type: Boolean,
+        default: true,
+    },
+});
+
+const emit = defineEmits(['toggle']);
+
+const toggle = () => {
+    if (props.closeable) {
+        emit('toggle');
+    }
+};
+
+const isSubmenuVisible = ref({
+    dashboard: false,
+});
+
+const toggleList = (menu) => {
+    isSubmenuVisible.value[menu] = !isSubmenuVisible.value[menu];
+};
+
+watch(
+    () => {
+        if (route().current('dashboard')) {
+            isSubmenuVisible.value.dashboard = true;
+        }
+        else if (route().current('user.index') || route().current('role.index')) {
+            isSubmenuVisible.value.user = true;
+        }
+        else if (route().current('shipment.index') || route().current('invoice.index')) {
+            isSubmenuVisible.value.shipment = true;
+        }
+        else if (route().current('ledger.index') || route().current('ledger.company')) {
+            isSubmenuVisible.value.ledger = true;
+        }
+    }
+);
+
 </script>
 
 <template>
@@ -12,56 +56,72 @@ const permission = usePage().props.can;
             <div>
                 <h4 class="logo-text">ESC Portal</h4>
             </div>
-            <div class="toggle-icon ms-auto"><i class='bx bx-arrow-back'></i>
+            <div class="toggle-icon ms-auto" @click="toggle"><i class='bx bx-arrow-back'></i>
             </div>
         </div>
         <!--navigation-->
         <ul class="metismenu" id="menu">
-            <li v-if="permission.dashboard">
-                <a href="javascript:;" class="has-arrow">
+            <li :class="{ 'mm-active': route().current('dashboard') }" v-if="permission.dashboard">
+                <a href="javascript:;" class="has-arrow" @click="toggleList('dashboard')">
                     <div class="parent-icon"><i class='bx bx-home-alt'></i>
                     </div>
                     <div class="menu-title">Dashboard</div>
                 </a>
-                <ul>
-                    <li v-if="permission.dashboard">
+                <ul :class="{ 'hidden': !isSubmenuVisible.dashboard }">
+                    <li :class="{ 'mm-active': route().current('dashboard') }" v-if="permission.dashboard">
                         <Link :href="route('dashboard')"><i class='bx bx-radio-circle'></i>Analytics</Link>
                     </li>
                 </ul>
             </li>
+
             <!-- <li class="menu-label">Application</li> -->
-            <li v-if="permission.user_list || permission.role_list">
-                <a href="javascript:;" class="has-arrow">
-                    <div class="parent-icon"><i class="bx bx-category"></i>
+
+            <li v-if="permission.user_list || permission.role_list"
+                :class="{ 'mm-active': route().current('user.index') || route().current('role.index') }">
+                <a href="javascript:;" class="has-arrow" @click="toggleList('user')">
+                    <div class="parent-icon"><i class="bx bx-user"></i>
                     </div>
                     <div class="menu-title">Manage Users</div>
                 </a>
-                <ul>
-                    <li v-if="permission.user_list">
+                <ul :class="{ 'hidden': !isSubmenuVisible.user }">
+                    <li v-if="permission.user_list" :class="{ 'mm-active': route().current('user.index') }">
                         <Link :href="route('user.index')"><i class='bx bx-radio-circle'></i>List Users</Link>
                     </li>
-                    <li v-if="permission.role_list">
+                    <li v-if="permission.role_list" :class="{ 'mm-active': route().current('role.index') }">
                         <Link :href="route('role.index')"><i class='bx bx-radio-circle'></i>Role & Permissions</Link>
                     </li>
                 </ul>
             </li>
-            <li v-if="permission.shipment_list || permission.invoice_list || ledger_list">
-                <a href="javascript:;" class="has-arrow">
-                    <div class="parent-icon"><i class="bx bx-printer"></i>
+
+            <li v-if="permission.shipment_list || permission.invoice_list"
+                :class="{ 'mm-active': route().current('shipment.index') || route().current('invoice.index') }">
+                <a href="javascript:;" class="has-arrow" @click="toggleList('shipment')">
+                    <div class="parent-icon"><i class="bx bx-box"></i>
                     </div>
                     <div class="menu-title">Manage Shipments</div>
                 </a>
-                <ul>
-                    <li v-if="permission.shipment_list">
+                <ul :class="{ 'hidden': !isSubmenuVisible.shipment }">
+                    <li v-if="permission.shipment_list" :class="{ 'mm-active': route().current('shipment.index') }">
                         <Link :href="route('shipment.index')"><i class='bx bx-radio-circle'></i>Pre-Shipments</Link>
                     </li>
-                    <li v-if="permission.invoice_list">
+                    <li v-if="permission.invoice_list" :class="{ 'mm-active': route().current('invoice.index') }">
                         <Link :href="route('invoice.index')"><i class='bx bx-radio-circle'></i>List Invoices</Link>
                     </li>
-                    <li v-if="permission.ledger_list">
+                </ul>
+            </li>
+
+            <li v-if="permission.ledger_list || permission.ledger_company"
+                :class="{ 'mm-active': route().current('ledger.index') || route().current('ledger.company') }">
+                <a href="javascript:;" class="has-arrow" @click="toggleList('ledger')">
+                    <div class="parent-icon"><i class="bx bx-printer"></i>
+                    </div>
+                    <div class="menu-title">Manage Ledgers</div>
+                </a>
+                <ul :class="{ 'hidden': !isSubmenuVisible.ledger }">
+                    <li v-if="permission.ledger_list" :class="{ 'mm-active': route().current('ledger.index') }">
                         <Link :href="route('ledger.index')"><i class='bx bx-radio-circle'></i>General Ledger</Link>
                     </li>
-                    <li v-if="permission.ledger_company">
+                    <li v-if="permission.ledger_company" :class="{ 'mm-active': route().current('ledger.company') }">
                         <Link :href="route('ledger.company')"><i class='bx bx-radio-circle'></i>Company Ledger</Link>
                     </li>
                 </ul>
@@ -71,3 +131,38 @@ const permission = usePage().props.can;
         <!--end navigation-->
     </div>
 </template>
+
+<style>
+.sidebar-wrapper {
+    overflow-y: hidden;
+    transition: overflow-y 0.3s ease;
+}
+
+.sidebar-wrapper:hover {
+    overflow-y: auto;
+}
+
+/* width */
+::-webkit-scrollbar {
+    width: 4px;
+    height: 4px;
+}
+
+/* Track */
+::-webkit-scrollbar-track {
+    background: transparent
+}
+
+/* Handle */
+::-webkit-scrollbar-thumb {
+    background: #888;
+    border-radius: 10px;
+    /* Add radius */
+
+}
+
+/* Handle on hover */
+::-webkit-scrollbar-thumb:hover {
+    background: rgb(204, 201, 201);
+}
+</style>
