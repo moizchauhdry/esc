e9413beadc6871682f7d9148e5a327e9a3fe620c
moiz.akhtar <moiz.akhtar@pitb.gov.pk>
2024-11-04 18:32:19 +0500

    due carrier and net total fixes

--- app/Http/Controllers/ReportController.php
+++ app/Http/Controllers/ReportController.php
@@ -37,7 +37,7 @@ class ReportController extends Controller
         session(['filter' => $filter]);
 
         $query = Invoice::select('*');
-        $query->selectRaw('(total - net_rate) as net_total');
+        $query->selectRaw('(total - net_payable) as net_total');
 
         $query->when($filter['from'] && $filter['to'], function ($q) use ($filter) {
             $q->whereDate('invoice_at', '>=', $filter['from']);
@@ -94,9 +94,9 @@ class ReportController extends Controller
         $validate = $request->validate($rules);
 
         $data = [
-            'due_carrier' => $request->due_carrier,
-            'net_rate' => $request->net_rate,
-            'net_payable' => $request->due_carrier * $request->net_rate,
+            'due_carrier' => $request->due_carrier ?? 0,
+            'net_rate' => $request->net_rate ?? 0,
+            'net_payable' => $request->due_carrier + $request->net_rate,
         ];
 
         $invoice = Invoice::find($invoice_id);
